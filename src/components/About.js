f076e86fc955f6d005fc95e944ca4e7646419b4b
Jason Cabot <51163690+jasoncabot-ms@users.noreply.github.com>
2020-07-02 16:17:28 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div className="container mt-5">
             <div className="row">
-                About this site...
+                About this site to say something else...
             </div>
         </div>
     );
